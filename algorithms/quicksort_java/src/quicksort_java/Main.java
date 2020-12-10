2add5e47b0b641f523612316940f12a3ec75ca41
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-12-10 09:14:35 +0200

    quicksort - upd for java main

--- algorithms/quicksort_java/src/quicksort_java/Main.java
+++ algorithms/quicksort_java/src/quicksort_java/Main.java
@@ -3,7 +3,7 @@ package quicksort_java;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    static int[] randomFill(int[] arr)
+    static int[] fillArray(int[] arr)
     {
         for(int i = 0; i < arr.length ; i++){
             arr[i] = ThreadLocalRandom.current().nextInt(0, arr.length + 1);
@@ -12,18 +12,23 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	    int n = 100000;
-        int[] array01 = new int[n];
-        randomFill(array01);
+	    int size = 1000;
+        int[] array01 = new int[size];
+        fillArray(array01);
         int[] array02 = array01.clone();
 
         double startTimeLomuto = System.nanoTime();
-        LomutoQuicksort.quickSort(array01, 0, n-1);
+        LomutoQuicksort.quickSort(array01, 0, size-1);
         double stopTimeLomuto = System.nanoTime();
 
         double startTimeHoare = System.nanoTime();
-        HoareQuicksort.quickSort(array02, 0, n-1);
+        HoareQuicksort.quickSort(array02, 0, size-1);
         double stopTimeHoare = System.nanoTime();
+
+        System.out.printf("Number of elements: \t %d \n", size);
+
+        LomutoQuicksort.printArray(array01, size);
+        HoareQuicksort.printArray(array01, size);
 
         System.out.printf("Execution Time Lomuto: \t %.2f ms \n", (stopTimeLomuto - startTimeLomuto) / 1000000);
         System.out.printf("Execution Time Hoare: \t %.2f ms \n", (stopTimeHoare - startTimeHoare) / 1000000);
