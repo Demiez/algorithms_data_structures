99f412380f7da996192f4f37e0477e3d4fe3218d
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-12-13 15:35:47 +0200

    queue - added queue

---
+++ data_structures/queue/queue.ts
@@ -0,0 +1,23 @@
+export class Queue {
+  private elements: Array<number | string> = [];
+
+  public enqueue(el: number | string): void {
+    this.elements.push(el);
+  }
+
+  public dequeue(): number | string {
+    return this.elements.shift();
+  }
+
+  public peek(): number | string | undefined {
+    return !this.isEmpty ? this.elements[0] : undefined;
+  }
+
+  public length(): number {
+    return this.elements.length;
+  }
+
+  private isEmpty(): boolean {
+    return this.elements.length === 0;
+  }
+}
