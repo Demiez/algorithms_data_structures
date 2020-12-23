3bc99a0ba3a1842129e67c131ef7a16d4ecddfa1
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-12-23 07:45:42 +0200

    insertion - added max recursive method

--- algorithms/insertion_sort/insertion.sort.ts
+++ algorithms/insertion_sort/insertion.sort.ts
@@ -1,6 +1,8 @@
 import { Basic } from '../basic.class';
 
 export class InsertionSort extends Basic {
+  public numberOfRecursions: number = 0;
+
   constructor(arr: Array<number>) {
     super(arr);
   }
@@ -22,6 +24,34 @@ export class InsertionSort extends Basic {
     if (n <= 1) return;
 
     this.sortRecursive(arr, n - 1);
+
+    const last = arr[n - 1];
+    let j = n - 2;
+
+    while (j >= 0 && arr[j] > last) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = last;
+  }
+
+  sortRecursiveMax(arr = this.arr, n = this.arr.length) {
+    if (n <= 1) return;
+    this.numberOfRecursions++;
+
+    this.numberOfRecursions % 1000 === 0
+      ? setImmediate(() => {
+          this.sortRecursive(arr, n - 1);
+        })
+      : this.sortRecursive(arr, n - 1);
+
+    // if (this.numberOfRecursions % 1000 === 0) {
+    //   setImmediate(() => {
+    //     this.sortRecursive(arr, n - 1);
+    //   });
+    // } else {
+    //   this.sortRecursive(arr, n - 1);
+    // }
 
     const last = arr[n - 1];
     let j = n - 2;
--- algorithms/insertion_sort/main.ts
+++ algorithms/insertion_sort/main.ts
@@ -1,18 +1,27 @@
 import { InsertionSort } from './insertion.sort';
 import { fillArray, displayExecTime } from '../../utils/utils';
 
-const array = fillArray(8500);
+const array = fillArray(1000000);
 
 const array01 = new InsertionSort(array);
 const array02 = new InsertionSort(array);
+const array03 = new InsertionSort(array);
 
 const startTimeNonRecursive = process.hrtime();
 array01.sort();
 const endTimeNonRecursive = process.hrtime(startTimeNonRecursive);
 displayExecTime(endTimeNonRecursive, 'Insertion Sort - Non Recursive');
 
-const startTimeRecursive = process.hrtime();
-array02.sortRecursive();
-const endTimeRecursive = process.hrtime(startTimeRecursive);
+// Usual recursive won't work with large number of elements because of RangeError: Maximum call stack size exceeded
+// On over 5000 elements the difference with RecursiveMax is small (about 0,2 - 0,5 ms)
+// const startTimeRecursive = process.hrtime();
+// array02.sortRecursive();
+// const endTimeRecursive = process.hrtime(startTimeRecursive);
+// displayExecTime(endTimeRecursive, 'Insertion Sort - Recursive');
+
+const startTimeRecursiveMax = process.hrtime();
+array03.sort();
+const endTimeRecursiveMax = process.hrtime(startTimeRecursiveMax);
+displayExecTime(endTimeRecursiveMax, 'Insertion Sort - Recursive Max');
+
 // array02.printArray();
-displayExecTime(endTimeRecursive, 'Insertion Sort - Recursive');
